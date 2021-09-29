f5732ca9669cde90d55c91fb930ea419862b799a
BuildTools <unconfigured@null.spigotmc.org>
2021-09-29 13:12:55 +0200

    Fetching data from Github

--- js/default-functions.js
+++ js/default-functions.js
@@ -1,3 +1,34 @@
+var get = window.location.search.substr(1).split("&");
+var categories = ["internet", "system", "multimedia", "games", "science & education", "navigation", "connectivity", "reading", "writing", "time", "security", "theming", "development", "sports & health", "money", "phone & sms", "graphics"];
+
+for(let i = 0; i < categories.length; i++){
+    let jsonApp = localStorage.getItem(categories[i]);
+    let jsonTime = localStorage.getItem(categories[i] + "-time");
+
+    if(typeof(jsonApp) == 'undefined' || jsonApp == null){
+        fetchAppData(categories[i]);
+        continue;
+    }
+    if(typeof(jsonTime) == 'undefined' || jsonTime == null){
+        fetchAppData(categories[i]);
+        continue;
+    }
+   
+    try{
+        JSON.parse(jsonApp);
+    }catch (e){
+        localStorage.setItem(category, "{}");
+        localStorage.setItem(category + "-time", 0);
+        fetchAppData(categories[i]);
+        continue;
+    }
+
+    if(jsonTime + 86400000 < Date.now()){
+        fetchAppData(categories[i]);
+        continue;
+    }
+}
+
 function hide(element){
     document.getElementById(element).style.visibility = 'hidden';
 }
@@ -20,6 +51,32 @@ function toggleMenu(){
     }else{
         document.getElementById("main-menu-mobile").style.visibility = 'hidden';
     }
+}
+
+function fetchAppData(category){
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://raw.githubusercontent.com/Rabbit-Company/RabbitStore-Server/main/categories/" + category + "/apps.json");
+
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhr.onreadystatechange = function () {
+
+        if(xhr.readyState === 4){
+            if(xhr.status != 200) return;
+            try {
+                JSON.parse(xhr.responseText);
+                localStorage.setItem(category, xhr.responseText);
+                localStorage.setItem(category + "-time", Date.now());
+            } catch (e) {
+                localStorage.setItem(category, "{}");
+                localStorage.setItem(category + "-time", 0);
+            }
+        }
+
+    };
+    xhr.send();
 }
 
 function copyToClipboard(text){
--- js/index.js
+++ js/index.js
@@ -1,5 +1,37 @@
 document.getElementById("main-menu-mobile").style.visibility = 'hidden';
 
+for(let i = 0; i < get.length; i++){
+    let data = get[i].split("=");
+    if(data[0] == "category"){
+        if(!categories.includes(decodeURIComponent(data[1]))) window.location = "?category=internet";
+        document.getElementById("category").innerHTML = decodeURIComponent(data[1].toUpperCase());
+        document.getElementById("desktop-menu-" + data[1]).className = "bg-gray-900 text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md";
+        document.getElementById("mobile-menu-" + data[1]).className = "bg-gray-900 text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md";
+
+        let json = JSON.parse(localStorage.getItem(data[1]));
+        let html = "";
+        for(let i = 0; i < Object.keys(json).length; i++){
+            let id = Object.keys(json)[i];
+            html += "<li class='col-span-1 text-gray-300 rounded-lg shadow divide-y divide-gray-200 bg-gray-800'>";
+            html += "<a href='?app=" + id + "'>";
+            html += "<div class='w-full text-center justify-between p-6'>";
+            html += "<img class='w-24 h-24 bg-gray-700 rounded-full mx-auto' src='" + json[id].icon + "' alt='" + json[id].name + "'>"
+            html += "<h1 class='text-2xl mt-2'>" + json[id].name + "</h1>";
+            html += "<span class='text-gray-500'>" + json[id].description.short + "</span></div></a></li>";
+        }
+        document.getElementById("app-list").innerHTML = html;
+    }else if(data[0] == "app"){
+        for(let i = 0; i < categories.length; i++){
+            let json = JSON.parse(localStorage.getItem(categories[i]));
+            if(Object.keys(json).includes(data[1])){
+                document.getElementById("category").innerText = json[data[1]].name;
+                break;
+            }
+        }
+    }
+
+}
+
 document.getElementById("open-main-menu-btn").addEventListener('click', () => {
     toggleMenu();
 });
